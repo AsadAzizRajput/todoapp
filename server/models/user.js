fffd360341288f0f7df413953bbab3d94c156975
AsadAzizRajput <asadazizjs@gmail.com>
2018-02-15 14:37:50 +0500

    Middle ware added

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const validator = require('validator');
 const {Schema} = mongoose;
 const  jwt = require('jsonwebtoken');
 const _ =  require('lodash');
+const bcrypt = require('bcryptjs')
 
 
 var userSchema = new Schema({
@@ -71,7 +72,20 @@ userSchema.statics.findByToken =function(token){
     })
 }
 
+userSchema.pre('save',function(next){
+    var user = this;
+    if(user.isModified('password'))
+    {
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+                user.password = hash;
+                next();
+            });
+        });
+    }else{
 
+    }
+})
 
 var user = mongoose.model('user',userSchema);
 
